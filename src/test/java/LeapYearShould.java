a56f5aaf66bd493bb9839712d0598746d700f1c9
Sstark97 <aitor@leanmind.es>
2023-10-17 12:50:39 +0100

    Test(:rotating_light:):is_not_a_leap_year

---
+++ src/test/java/LeapYearShould.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class LeapYearShould {
+    @Test
+    public void is_not_a_leap_year() {
+        assertFalse(isLeapYear(0));
+    }
+
+    private boolean isLeapYear(int year) {
+        return true;
+    }
+}
