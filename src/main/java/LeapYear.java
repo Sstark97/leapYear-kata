2d72cd49a4b9f8d6d60a80a776be608e0a359197
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-18 14:31:59 +0200

    :recycle: Extract conditions LeapYear class in methods

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,13 +1,26 @@
 public class LeapYear {
     public static LeapYear of(int year) throws LeapYearException {
-        if(year % 4 != 0) {
+        if(!isDivisibleByFour(year)) {
             throw new LeapYearException(year + " is not leap year because is not divisible by 4");
         }else if(isNotLeapYear(year)){
             throw new LeapYearException(year + " is not leap year because is divisible by 4 and 100 but not by 400");
         }
         return null;
     }
+    // TODO: Rename this method
     private static boolean isNotLeapYear(int year) {
-        return year % 4 == 0 && year % 100 == 0 && year % 400 != 0;
+        return isDivisibleByFour(year) && isDivisibleByOneHundred(year) && isNotDivisibleByFourHundred(year);
+    }
+
+    private static boolean isNotDivisibleByFourHundred(int year) {
+        return year % 400 != 0;
+    }
+
+    private static boolean isDivisibleByOneHundred(int year) {
+        return year % 100 == 0;
+    }
+
+    private static boolean isDivisibleByFour(int year) {
+        return year % 4 == 0;
     }
 }
