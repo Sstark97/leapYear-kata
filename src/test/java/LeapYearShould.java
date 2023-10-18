a5c1f1c0cf5b457b1ce7da7013fba5ff62ccb2b3
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-18 11:25:12 +0200

    :recycle: Add more values to parametrized test

--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -6,14 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearShould {
-    @ParameterizedTest
-    @ValueSource(ints = {2, 3, 2001, 2005, 3035, 4099, 15343})
+    @ParameterizedTest(name="Error element {index}: {0} is a leap year")
+    @ValueSource(ints = {2, 3, 427, 431, 435, 439, 1997, 2001, 2005, 2335, 2339, 2343, 2347, 2351, 2355, 2359, 2363,
+            2367, 2371, 2375, 2379, 2383, 2387, 2391, 2395, 2399, 3035, 4099, 4437, 15343})
     public void is_not_a_leap_year(int year) {
         assertFalse(isLeapYear(year));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {4, 8, 12, 16, 1840, 1836, 2024, 3044, 4444, 8888, 16444})
+    @ValueSource(ints = {4, 8, 12, 16, 1840, 1836, 1996, 2024, 3044, 4444, 8888, 16444, 2028, 2032, 2036, 2040, 2044,
+            2048, 2052, 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084,2340, 2344, 2348, 2352, 2356, 2360, 2364, 2368,
+            2372, 2376, 2380, 2384, 2388, 2392, 2396, 2400})
     void is_a_leap_year(int year) {
         assertTrue(isLeapYear(year));
     }
