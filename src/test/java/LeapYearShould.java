2aab9883fe8fc01ca485f8e834ea907f9211864f
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-18 09:37:36 +0200

    :recycle: replace given year to one in the first test

--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class LeapYearShould {
     @Test
     public void is_not_a_leap_year() {
-        assertFalse(isLeapYear(0));
+        assertFalse(isLeapYear(1));
     }
 
     private boolean isLeapYear(int year) {
         return false;
     }
-}
+//  A year is a leap year if it is divisible by 4.
